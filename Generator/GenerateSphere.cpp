e47bd0d4a4bc417313dd6e44cb22df035103f078
andrepinto42 <andrepinto.goncalves@gmail.com>
2022-03-12 16:57:00 +0000

    Sphere incorporada no generator

---
+++ Generator/GenerateSphere.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pinto on 12/03/22.
+//
+
+#include "GenerateSphere.h"
---
+++ Generator/GenerateSphere.h
@@ -0,0 +1,14 @@
+//
+// Created by pinto on 12/03/22.
+//
+
+#ifndef MAIN_CPP_GENERATESPHERE_H
+#define MAIN_CPP_GENERATESPHERE_H
+
+
+class GenerateSphere {
+
+};
+
+
+#endif //MAIN_CPP_GENERATESPHERE_H
